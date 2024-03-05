8b20f50d2b74f9e0c019bf044f1283ea4f8b02be
Oscar Andrade <u20211196841@usco.edu.co>
2024-03-04 23:39:05 -0500

    Agregado la imagen y nombre centrados

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import sung from "./assets/sung.jpg"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <div class="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold ">
+        Oscar Andrade
+      </h1>
+      <img className="w-48 h-auto" src={sung} alt="Sung Jin Woo" />
+    </div>
+
+      
+    </> 
   )
 }
